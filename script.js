064466c7ffc61aa9240c6b452dc7cbd9b5d75d04
Alex Blanco <53310027+coolbeansblanco@users.noreply.github.com>
2023-06-26 03:36:39 -0600

    add operate function for evaluating

--- script.js
+++ script.js
@@ -4,23 +4,36 @@ const multiply = document.getElementById('multiply');
 const divide = document.getElementById('divide');
 const equal = document.getElementById('equals');
 
+let operand1, operand2, operator;
+
 add?.addEventListener("click", () => {
-    console.log("Adding");
+    operator = "+";
 });
 
 subtract?.addEventListener("click", () =>{
-    console.log("Subtracting");
+    operator = "-";
 });
 
 multiply?.addEventListener("click", () =>{
-    console.log("Multiplying");
+    operator = "*";
 });
 
 divide?.addEventListener("click", () =>{
-    console.log("Dividing");
+    operator = "/";
 });
 
 function add(num1, num2) {return num1 + num2;}
 function subtract(num1, num2) {return num1 - num2;}
 function multiply(num1, num2) {return num1 * num2;}
 function divide(num1, num2) {return num1 / num2;}
+
+function operate(){
+    if (operator === "+")
+        return add(operand1, operand2);
+    if (operator === "-")
+        return subtract(operand1, operand2);
+    if (operator === "*")
+        return multiply(operand1, operand2);
+    if (operator === "/")
+        return divide(operand1, operand2);
+}
