c04ae7940ca68508d9bee2a50fa52e3b0013f7ad
Alex Blanco <53310027+coolbeansblanco@users.noreply.github.com>
2023-06-26 04:39:15 -0600

    add logic to listeners and update display

--- script.js
+++ script.js
@@ -1,32 +1,54 @@
-const add = document.getElementById('add');
-const subtract = document.getElementById('subtract');
-const multiply = document.getElementById('multiply');
-const divide = document.getElementById('divide');
-const equal = document.getElementById('equals');
-const clear = document.getElementById('clear');
+const addBtn = document.getElementById('add');
+const subtractBtn = document.getElementById('subtract');
+const multiplyBtn = document.getElementById('multiply');
+const divideBtn = document.getElementById('divide');
+const equalBtn = document.getElementById('equals');
+const clearBtn = document.getElementById('clear');
+const display = document.getElementById('display');
 
-let operand1, operand2, operator;
+let operand1; 
+let operand2;
+let operator;
+let displayValue ="";
 
-add?.addEventListener("click", () => {
+for (let i=0; i<10; i++){
+    document.getElementById(i).addEventListener("click", (textContent) => {
+        displayValue += i.toString();
+        display.textContent = displayValue;
+    });
+}
+
+addBtn?.addEventListener("click", () => {
+    operand1 = displayValue;
+    displayValue = "";
     operator = "+";
 });
 
-subtract?.addEventListener("click", () =>{
+subtractBtn?.addEventListener("click", () =>{
+    operand1 = displayValue;
+    displayValue = "";
     operator = "-";
 });
 
-multiply?.addEventListener("click", () =>{
+multiplyBtn?.addEventListener("click", () =>{
+    operand1 = displayValue;
+    displayValue = "";
     operator = "*";
 });
 
-divide?.addEventListener("click", () =>{
+divideBtn?.addEventListener("click", () =>{
+    operand1 = displayValue;
+    displayValue = "";
     operator = "/";
 });
 
-equal?.addEventListener("click", operate)
+equalBtn?.addEventListener("click", operate)
 
-clear?.addEventListener("click", () =>{
-
+clearBtn?.addEventListener("click", () =>{
+    operand1 = undefined;
+    operand2 = undefined;
+    operator = undefined;
+    displayValue = "";
 })
 
 function add(num1, num2) {return num1 + num2;}
@@ -34,13 +56,19 @@ function subtract(num1, num2) {return num1 - num2;}
 function multiply(num1, num2) {return num1 * num2;}
 function divide(num1, num2) {return num1 / num2;}
 
+function clearDisplay(){
+    
+}
+
 function operate(){
+    operand2 = Number(displayValue);
     if (operator === "+")
-        return add(operand1, operand2);
+        display.textContent = add(operand1, operand2);
     if (operator === "-")
-        return subtract(operand1, operand2);
+        display.textContent = subtract(operand1, operand2);
     if (operator === "*")
-        return multiply(operand1, operand2);
+        display.textContent = multiply(operand1, operand2);
     if (operator === "/")
-        return divide(operand1, operand2);
+        display.textContent = divide(operand1, operand2);
+    displayValue = "";
 }
